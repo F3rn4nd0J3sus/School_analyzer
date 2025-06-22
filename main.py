feb93f5f19a3a1f9643cbc6d599805fcf34e3e15
F3rn4nd0J3sus <126753451+F3rn4nd0J3sus@users.noreply.github.com>
2025-06-21 22:16:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import streamlit as st
 page_1 = st.Page("Pages/page_1.py", title="Cargar base de datos", icon="📂")
 page_2 = st.Page("Pages/page_2.py", title="Análisis de género", icon="♀️")
 page_3 = st.Page("Pages/page_3.py", title="Análisis estado/provincia", icon="🌎")
+page_4 = st.Page("Pages/page_4.py", title="Análisis nivel/grado académico", icon="🎓")
+page_5 = st.Page("Pages/page_5.py", title="Análisis categoría/tipo", icon="🏷️")
 
 # Configura la navegación
-pg = st.navigation([page_1, page_2, page_3])
+pg = st.navigation([page_1, page_2, page_3, page_4, page_5])
 
 # Ejecuta la página seleccionada
 pg.run()
